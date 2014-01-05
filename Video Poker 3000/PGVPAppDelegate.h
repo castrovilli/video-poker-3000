c4c34d424895246d3652280b3c654428aece4cfc
Paul Griffiths <mail@paulgriffiths.net>
2014-01-04 20:22:13 -0500

    Initial Commit

---
+++ Video Poker 3000/PGVPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGVPAppDelegate.h
+//  Video Poker 3000
+//
+//  Created by Paul Griffiths on 1/4/14.
+//  Copyright (c) 2014 Paul Griffiths. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGVPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Video Poker 3000/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Video Poker 3000
+//
+//  Created by Paul Griffiths on 1/4/14.
+//  Copyright (c) 2014 Paul Griffiths. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PGVPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PGVPAppDelegate class]));
+    }
+}
